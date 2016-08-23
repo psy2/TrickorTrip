39d9b4f888b8c7f5a55059d7cfae366394b104a5
psy <sogehao@gmail.com>
2016-08-23 16:28:42 +0900

    채팅구현함!! 대신 아직 미국밖에 안됨

--- TrickorTrip/WebContent/js/chatindex.js
+++ TrickorTrip/WebContent/js/chatindex.js
@@ -3,24 +3,57 @@
  */
 var websocket;
 
-$(document).ready(function() {
-    websocket=new WebSocket("ws://localhost:8080/chat/wsServer");
 
-    websocket.onmessage=function(result){
-        var txt=$("#myTextArea").val();
-        $("#myTextArea").val(txt+"\n"+result.data);
-    }
+$( function() {
+    $( "#dialog" ).dialog({
+        autoOpen: false,
+        close:function (event, ui) {
+            websocket.close();
+        }
+    });
 
-})
+    $( "#us_submit" ).on( "click", function() {
+        $( "#dialog" ).dialog( "open" );
 
-function closeServer(){
-    websocket.close();
+    });
+} );
 
+function sendMSG1(){
+
+    websocket.send($("#myText").val());
+    $("#myText").val("");
 }
+
+
+
 function sendMSG(){
     //enter key를 입력했을 때 입력된 내용을 서버에 보내요!
     if(event.keyCode==13){
         //enter key가 입력되면 서버에 데이터를 보내요
         websocket.send($("#myText").val());
+        $("#myText").val("");
     }
+
+}
+
+
+function openChat(){
+
+
+    $('#dialog').dialog({
+            autoOpen: false,
+            position:'center'
+
+        });
+
+    $('#dialog').dialog('open');
+
+
+        websocket=new WebSocket("ws://localhost:8080/tot/wsServer");
+
+        websocket.onmessage=function(result){
+            var txt=$("#myTextArea").val();
+            $("#myTextArea").val(txt+"\n"+result.data);
+        }
+
 }
